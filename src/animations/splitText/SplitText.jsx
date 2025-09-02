3f7a0a7dedd95a7cda76e3a82bc20fcebfc7afd1
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-09-02 17:26:44 +0530

    btchd

--- src/animations/splitText/SplitText.jsx
+++ src/animations/splitText/SplitText.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SplitText = ({text,className = ''})=>(
     <>
     {text.split('').map((char,i)=>(
-        <span key={i} className={`reveal-char inline-block translate-y-[130%] ${className}`} aria-hidden='true'>
+        <span key={i} className={`reveal-char inline-block translate-y-[150%] ${className}`} aria-hidden='true'>
             {char === ' ' ? '\u00A0':char}
         </span>
     ))}
