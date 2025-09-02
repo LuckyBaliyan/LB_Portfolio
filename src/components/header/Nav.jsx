839942c10b0e22bd9a3ab304b5085b01c1c42e6c
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-09-02 17:49:41 +0530

    lp

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -47,7 +47,7 @@ const Nav = ({delay = 0.4}) => {
           <h6 ref={logoRef} className='translate-y-[100%]'>
             <b>L</b><b>B</b><sup className='text-3xl'>&copy;</sup>
           </h6>
-          <p className='md:inline-block opacity-0 text-sm !text-white translate-x-[-20%] md:translate-x-0 scale-60 md:scale-100 font-light self-center anim-placeholder'>
+          <p className='md:inline-block opacity-0 text-sm !text-white translate-x-[-20%] md:translate-x-0 scale-70 md:scale-100 font-light self-center anim-placeholder'>
             (web Developer / Designer)
           </p>
        </div>
