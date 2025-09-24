bce3593465d6360868c631999100bd0df681769d
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-09-24 20:51:21 +0530

    all Projects hover effect

--- src/components/buttons/MainBtn.jsx
+++ src/components/buttons/MainBtn.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { FaLocationArrow } from "react-icons/fa";
 
-const MainBtn = ({text,link='/works'}) => {
+const MainBtn = ({text,link='/works',custome=''}) => {
   return (
     <Link to={link}>
-        <button className='main-btn flex gap-3 items-center justify-center'>
+        <button className={`main-btn flex gap-3 items-center justify-center ${custome}`}>
             {text}
             <FaLocationArrow className='tex-md rotate-6'/>
         </button>
--- src/components/sections/AllProjects.jsx
+++ src/components/sections/AllProjects.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import TextUP from '../../animations/Reavel/TextUP'
-import { allProjects } from '../../context/data'
+import { allProjects, latest } from '../../context/data'
 import { FaArrowRight } from "react-icons/fa";
 import MainBtn from '../buttons/MainBtn';
+import MovingCard from '../features/MovingCard';
+
+
 
 const AllProjects = () => {
+      const [pos, setPos] = useState({ x: 700, y: 100 });
+      const [isInside, setIsInside] = useState(false);
+      const [activeIndex,setActiveIndex] = useState(null);
+
+    const handleMouseMove = (e) => {
+           const rect = e.currentTarget.getBoundingClientRect();
+           const x = e.clientX - rect.left;
+           const y = e.clientY - rect.top;
+           setPos({ x, y });
+     };
+
   return (
    <div className='py-6'>
       <div className='flex flex-col gap-6'>
         <div className='self-center'>
             <TextUP text={'Projects'} secText={'Works'} className='text-center'/>
         </div>
-        <div className='w-full mt-6'>
+        <div onMouseMove={handleMouseMove} onMouseDownCapture={handleMouseMove}  onMouseEnter={()=>setIsInside(true)} onMouseLeave={()=>{setIsInside(false);setActiveIndex(null)}} className='w-full mt-6 relative'>
+         <MovingCard images={allProjects.map(p=> p.img)} activeIndex={activeIndex} x={pos.x} y={pos.y} opacity={isInside?1:0} />
          {allProjects.map((p,i)=>(
-           <div key={i} className='flex flex-col cursor-pointer lg:flex-row justify-between px-4 py-2 border-t-[1px]'>
+           <div key={i}  className='flex flex-col cursor-pointer lg:flex-row justify-between px-4 py-2 border-t-[1px]' onMouseEnter={()=>setActiveIndex(i)}>
             <div className='flex flex-col lg:flex-row  items-center justify-between lg:w-1/3'>
                 <h4 className='font-[satoshi] text-6xl font-semibold'>{p.Title}</h4>
                 <p className='text-3xl'>{p.Date}</p>
@@ -25,7 +40,7 @@ const AllProjects = () => {
            </div>
          ))}
         </div>
-        <div className='w-full flex justify-center items-center'>
+        <div className='w-full flex justify-center items-center z-[50]'>
             <MainBtn text={'View All'} />
         </div>
       </div>
