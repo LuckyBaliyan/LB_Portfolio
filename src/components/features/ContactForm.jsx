a328ff8720904d2350221da119699fdec3d293ca
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-10-04 20:34:04 +0530

    fix

--- src/components/features/ContactForm.jsx
+++ src/components/features/ContactForm.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import MainBtn from '../buttons/MainBtn'
 
 const ContactForm = () => {
+
+  const serviceId =   import.meta.env.VITE_EMAILJS_SERVICE_ID;
+  const templateID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+  const serviceKey =   import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+
+  console.log(serviceId,serviceKey,templateID);
+
   return (
     <form action="">
         <input type="text" placeholder='Name'/>
